223c38fd52ae05878dbde63f7c3746c6206b2ea0
Hee Hwang <heehcs@gmail.com>
2021-09-19 12:16:29 -0400

    Modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use chip8::Chip8;
+use std::fs::File;
+use std::io::Read;
+
+mod chip8;
+mod ram;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("data/INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+
+    //print!("Data: {:?}", data);
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
 }
