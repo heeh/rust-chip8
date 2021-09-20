80dc69cc4f342096837688c41409b5e1b7042396
Hee Hwang <heehcs@gmail.com>
2021-09-20 09:42:21 -0400

    Implement few cpu instructions

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod chip8;
 mod cpu;
 mod ram;
 
+#[allow(unused_must_use)]
 fn main() {
     let mut file = File::open("data/INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data);
 
-    //print!("Data: {:?}", data);
     let mut chip8 = Chip8::new();
     chip8.load_rom(&data);
 
