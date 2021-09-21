672a0a0cf76d0700e0d1e5a1cb173d33a65ee7e4
Hee Hwang <heehcs@gmail.com>
2021-09-21 12:43:15 -0400

    Get window working

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use crate::chip8::Chip8;
 use std::fs::File;
 use std::io::Read;
 
+use crate::display::Display;
+
 mod chip8;
 mod cpu;
 mod ram;
@@ -23,15 +25,19 @@ fn main() {
     chip8.load_rom(&data);
 
 
-		let WIDTH = 640;
-		let HEIGHT = 320;
+		let width = 640;
+		let height = 320;
 		
-		let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+		let mut buffer: Vec<u32> = vec![0; width * height];
 
+		// for i in buffer.iter_mut() {
+		// 		*i = 0xffff0000;
+		// }
+		
     let mut window = Window::new(
         "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
+        width,
+        height,
         WindowOptions::default(),
     )
     .unwrap_or_else(|e| {
@@ -42,10 +48,25 @@ fn main() {
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
+				chip8.run_instruction();
+				let chip8_buffer = chip8.get_display_buffer();
+
+				for y in 0..height {
+						for x in 0..width {
+								let index = y/10 * width/10 + x/10;
+								let pixel = chip8_buffer[index];
+								let color_pixel = match pixel {
+										0 => 0x0,
+										1 => 0xffffff,
+										_ => unreachable!()
+								};
+								buffer[y * width + x] = color_pixel;
+						}
+				}
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .update_with_buffer(&buffer, width, height)
             .unwrap();
     }
 }
