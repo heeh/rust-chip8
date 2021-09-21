8e7555be4337d81a4f60b4ecdd92933540625d15
Hee Hwang <heehcs@gmail.com>
2021-09-21 11:58:01 -0400

    Add minifb

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use self::chip8::Chip8;
+extern crate minifb;
+
+use minifb::{Key, Window, WindowOptions};
+
+use crate::chip8::Chip8;
 use std::fs::File;
 use std::io::Read;
 
@@ -18,7 +22,33 @@ fn main() {
     let mut chip8 = Chip8::new();
     chip8.load_rom(&data);
 
-    loop {
-        chip8.run_instruction();
+
+		let WIDTH = 64;
+		let HEIGHT = 32;
+		
+		let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
     }
 }
